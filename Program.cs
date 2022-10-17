6705175bb6307d32269d2c4a342a40148e82b6fd
FARUQESAY <113195467+faruqesay2@users.noreply.github.com>
2022-10-17 20:02:20 +0530

    Demo Repository

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Ball
+{
+    string color = "black";
+
+    static void Main(string[] args)
+    {
+        Ball myObj = new Ball();
+        Console.WriteLine(myObj.color);
+    }
+}
